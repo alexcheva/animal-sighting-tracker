1520a90a860b9a3c0a296d8b9ec26b6147088fb5
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-25 10:44:59 -0700

    add more styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div>Designed and Developed by Alexandra Lukinicheva.</div>
-        <a href='https://www.freepik.com/photos/animals'>Animals photos created by freepik - www.freepik.com</a>
+        <div>Designed and Developed by <a href="https://github.com/alexcheva">Alexandra Lukinicheva</a></div>
+        Animals photos created by <a href='https://www.freepik.com/photos/animals'>freepik - www.freepik.com</a>
         <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </div>
     </footer>
